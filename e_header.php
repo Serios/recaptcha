bf972a47a68040f6be5205bc4d09d2174d9df349
Serios <seriosbg@gmail.com>
2017-12-08 09:27:12 +0200

    Adding language support
    A little change to the code that allows for captcha to be rendered in the appropriate site language or fallback to English if CORE_LC  is not defined

--- e_header.php
+++ e_header.php
@@ -1,11 +1,12 @@
 <?php 
 $recaptchaActive = e107::pref('recaptcha', 'active');
 $recaptchaSiteKey = e107::pref('recaptcha', 'sitekey'); 
+$lang = ( defined('CORE_LC') ) ? '&hl='.CORE_LC : '&hl=en';
 
 		if($recaptchaActive)
 		{
  
-  e107::js("footer-inline", '<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
+  e107::js("footer-inline", '<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit'.$lang.'"
 		        async defer>
 		    </script>' );
 		 
